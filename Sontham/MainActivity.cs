d841a36275c916c0ff2694af19153af49e07754b
Akilan <thamarai.akilan@gmail.com>
2017-07-04 22:22:36 +0530

    Base project added

---
+++ Sontham/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Sontham
+{
+    [Activity(Label = "SonthaPantham", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+       //Button buttonLogin;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+          //  buttonLogin = FindViewById<Button>;
+           
+        }
+    }
+}
+
